9ed278f74e55fa4c5e0b5c270c0d80695a1cb672
aureliano <aureliano.franca@hotmail.com>
2015-11-13 17:54:29 -0200

    Create version meta-data controller.

---
+++ lib/support/version.rb
@@ -0,0 +1,14 @@
+module RamlPoliglota
+  module Support
+    
+    VERSION_NUMBERS = [
+      VERSION_MAJOR = 0,
+      VERSION_MINOR = 1,
+      VERSION_BUILD = 0
+    ]
+    
+    RELEASE_VERSION = VERSION_NUMBERS.join(".")
+    RELEASE_DATE = '2014-07-25'
+
+  end
+end
