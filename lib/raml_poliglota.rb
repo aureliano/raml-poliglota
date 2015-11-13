1f5c069791fda5327f2d55f6b861e02bd2d2f895
aureliano <aureliano.franca@hotmail.com>
2015-11-13 18:21:04 -0200

    Create lib main class.

--- lib/raml_poliglota.rb
+++ lib/raml_poliglota.rb
@@ -2,6 +2,15 @@ require File.expand_path '../support/requires.rb', __FILE__
 
 RamlPoliglota::Support::SourceLoader.new.load_project_source_files
 
-include RamlPoliglota::Support
+module RamlPoliglota
 
-command = command_line_options
+  class RamlPoliglota
+
+    def initialize
+      yield self if block_given?
+    end
+
+    attr_accessor :execution
+    
+  end
+end
