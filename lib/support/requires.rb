82e3b09c90c8fe57fc0a9b32e314cd68e7bfcd5d
aureliano <aureliano.franca@hotmail.com>
2015-11-13 17:10:53 -0200

    Configure project source-code loader.

--- lib/support/requires.rb
+++ lib/support/requires.rb
@@ -1,1 +1,32 @@
 require 'logging'
+require 'optparse'
+
+module RamlPoliglota
+  module Support
+
+    class SourceLoader
+
+      def initialize
+        @ignorable_source_files = [File.join('support', 'requires.rb'), File.join('raml_poliglota.rb')]
+        @dir_order = ['configuration', 'model', 'support']
+        @lib_dir = File.dirname(__FILE__).sub /\/support$/, ''
+      end
+
+      def load_project_source_files
+        @dir_order.each do |dir|
+          Dir[File.join(@lib_dir, dir, '*.rb')].each {|source_file| _require_source_file source_file }
+        end
+      end
+
+      private
+      def _require_source_file(source_file)
+        found = @ignorable_source_files.select {|file| source_file.end_with? file }
+        return unless found.empty?
+
+        puts "Requiring source file #{source_file}"
+        require source_file
+      end
+    end
+
+  end
+end
