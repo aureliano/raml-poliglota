3abb814c46a7254b7b2f6c1e380e0f2ee197a3aa
aureliano <aureliano.franca@hotmail.com>
2015-11-15 11:35:30 -0200

    Call executor method.

--- lib/raml_poliglota.rb
+++ lib/raml_poliglota.rb
@@ -2,15 +2,27 @@ require File.expand_path '../support/requires.rb', __FILE__
 
 RamlPoliglota::Support::SourceLoader.new.load_project_source_files
 
+include RamlPoliglota::Configuration
+
 module RamlPoliglota
 
   class RamlPoliglota
 
     def initialize
+      @logger = AppLogger.create_logger self
       yield self if block_given?
     end
 
     attr_accessor :execution
-    
+
+    def execute
+      @logger.info "Initialize RAML client code generation."
+      @logger.info "RAML file path: #{@execution.raml_path}"
+      @logger.info "Target generation language: #{@execution.language}"
+      @logger.info "Target output: #{@execution.output}"
+
+
+      @logger.info "#{@execution.language} source-code generated to #{@execution.output}"
+    end
   end
 end
