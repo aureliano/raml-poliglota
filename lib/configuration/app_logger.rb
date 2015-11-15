ede4984b49a352467c61436352823c0c342598ce
aureliano <aureliano.franca@hotmail.com>
2015-11-15 11:35:12 -0200

    Configure logging.

--- lib/configuration/app_logger.rb
+++ lib/configuration/app_logger.rb
@@ -3,22 +3,12 @@ module RamlPoliglota
 
     class AppLogger
     
-      @@appenders = []
-
-      def initialize
-        if @@appenders.nil?
-          AppLogger.add_stdout_logger
-        end
-      end
-    
-      def self.add_stdout_logger
+      @@appenders = [
         Logging.appenders.stdout(
           :level => :info,
           :layout => Logging::Layouts::Pattern.new(:date_pattern => '%Y-%m-%d %H:%M:%S', :pattern => '[%d] %-5l -- %c : %m\n')
         )
-        
-        @@appenders << 'stdout' unless @@appenders.include? 'stdout'
-      end
+      ]
       
       def self.create_logger(source)
         if @@appenders.empty?
