ecdbefd7f2705504a0d546fbeba3fc08569d7dba
aureliano <aureliano.franca@hotmail.com>
2015-11-17 17:27:18 -0200

    Add method to build model class.

--- test/unit/code/builder/code_builder_test.rb
+++ test/unit/code/builder/code_builder_test.rb
@@ -2,11 +2,13 @@ require 'test/unit'
 require File.expand_path '../../../../assert_helper.rb', __FILE__
 require File.expand_path '../../../../../lib/support/supported_programming_languages.rb', __FILE__
 require File.expand_path '../../../../../lib/code/builder/code_builder.rb', __FILE__
+require File.expand_path '../../../../../lib/code/builder/java/java_builder.rb', __FILE__
 
 class CodeBuilderTest < Test::Unit::TestCase
   
   include RamlPoliglota::Support
   include RamlPoliglota::Code::Builder
+  #include RamlPoliglota::Code::Builder::Java  
 
   def test_create
     builder = CodeBuilder.create SUPPORTED_PROGRAMMING_LANGUAGES[:java]
