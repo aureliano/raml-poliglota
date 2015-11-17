48d582e27baaa0cd613adeef2b5164d62911d2a1
aureliano <aureliano.franca@hotmail.com>
2015-11-17 15:04:33 -0200

    Add method to dynamically create specialized code generator.

--- lib/generator/code_generator.rb
+++ lib/generator/code_generator.rb
@@ -3,6 +3,19 @@ module RamlPoliglota
 
     class CodeGenerator
 
+      include RamlPoliglota::Support
+      
+      attr_accessor :namespace
+
+      def self.create(language)
+
+      include RamlPoliglota::Support
+        case language.name
+          when SUPPORTED_PROGRAMMING_LANGUAGES[:java].name then JavaGenerator.new
+          else raise "Unsupported programming language #{language.name}."
+        end
+      end
+
       def generate(raml)
         raise "Method not implemented."
       end
--- test/unit/generator/code_generator_test.rb
+++ test/unit/generator/code_generator_test.rb
@@ -1,10 +1,17 @@
 require 'test/unit'
 require File.expand_path '../../../assert_helper.rb', __FILE__
+require File.expand_path '../../../../lib/support/supported_programming_languages.rb', __FILE__
 require File.expand_path '../../../../lib/generator/code_generator.rb', __FILE__
 
 class CodeGeneratorTest < Test::Unit::TestCase
   
+  include RamlPoliglota::Support
   include RamlPoliglota::Generator
+
+  def test_create
+    gen = CodeGenerator.create SUPPORTED_PROGRAMMING_LANGUAGES[:java]
+    assert_true gen.instance_of? JavaGenerator
+  end
 
   def test_generate
     gen = CodeGenerator.new
