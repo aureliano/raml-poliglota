e9d816f34f06391007098e3327d4f4d9b1ef9e90
aureliano <aureliano.franca@hotmail.com>
2015-11-19 16:13:15 -0200

    Allow logger level configuration.

--- lib/configuration/app_logger.rb
+++ lib/configuration/app_logger.rb
@@ -2,22 +2,29 @@ module RamlPoliglota
   module Configuration
 
     class AppLogger
-    
-      @@appenders = [
-        Logging.appenders.stdout(
-          :level => :info,
-          :layout => Logging::Layouts::Pattern.new(:date_pattern => '%Y-%m-%d %H:%M:%S', :pattern => '[%d] %-5l -- %c : %m\n')
-        )
-      ]
+
+      @@default_level = :info
+
+      def self.set_default_level(level)
+        if ['debug', 'info', 'warn', 'error'].include? level.to_s.downcase
+          @@default_level = level.to_sym
+        end
+      end
       
       def self.create_logger(source)
-        if @@appenders.empty?
-          puts 'WARNING! There is no appender specified.'
-          return nil
-        end
-        
         logger = Logging.logger[source ||= self]
-        logger.add_appenders @@appenders
+        logger.level = @@default_level
+
+        layout = Logging::Layouts::Pattern.new(
+          :date_pattern => '%Y-%m-%d %H:%M:%S',
+          :pattern => '[%d] %-5l -- %c : %m\n'
+        )
+
+        logger.add_appenders(
+          Logging.appenders.stdout(
+            :level => @@default_level, :layout => layout
+          )
+        )
         
         return logger
       end
