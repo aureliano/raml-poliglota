7dcdad1e94fd7fe698afd2ad51018e83ef64b7ad
aureliano <aureliano.franca@hotmail.com>
2015-11-27 13:53:16 -0200

    Code refactoring: break class parsing into methods.

--- lib/parser/data_schema_parser.rb
+++ lib/parser/data_schema_parser.rb
@@ -27,45 +27,52 @@ module RamlPoliglota
           raise "Data schema '#{@entity_name}' doesn't have the 'properties' hash defined." if hash['properties'].nil?
           raise "The data schema #{@entity_name}'s attribute 'properties' must be of type Hash." unless hash['properties'].instance_of? Hash
           
-          hash['properties'].each do |key, value|
-            c.add_attribute(AttributeMeta.new do |a|
-              a.name = key
-              a.type = value['type']
-              a.visibility = 'private'
-              
-              unless value['items'].nil?
-                a.generic_type = value['items']['$ref']
-                c.collection_model_key = a.name
-              end
-            end)
-          end
-
           c.attributes ||= []
-
-          unless hash['$relationship'].nil?
-            hash['$relationship'].each do |key, value|
-              c.add_attribute(AttributeMeta.new do |a|
-                if value['type'] == '$single'
-                  a.type = value['schema']
-                elsif value['type'] == '$collection'
-                  a.type = 'array'
-                  a.generic_type = value['schema']
-                else
-                  raise "Unsupported relationship type #{value['type']}. Expected one of [$single, $collection]"
-                end
-
-                a.name = key
-                a.visibility = 'private'
-                a.relationship = true
-              end)
-            end
-          end
+          _add_properties hash, c
+          _add_relationship hash, c
         end
 
         hash['language'] = @language
         hash['class'] = clazz
 
         hash
+      end
+
+      private
+      def _add_properties(hash, c)
+        hash['properties'].each do |key, value|
+          c.add_attribute(AttributeMeta.new do |a|
+            a.name = key
+            a.type = value['type']
+            a.visibility = 'private'
+            
+            unless value['items'].nil?
+              a.generic_type = value['items']['$ref']
+              c.collection_model_key = a.name
+            end
+          end)
+        end
+      end
+
+      def _add_relationship(hash, c)
+        unless hash['$relationship'].nil?
+          hash['$relationship'].each do |key, value|
+            c.add_attribute(AttributeMeta.new do |a|
+              if value['type'] == '$single'
+                a.type = value['schema']
+              elsif value['type'] == '$collection'
+                a.type = 'array'
+                a.generic_type = value['schema']
+              else
+                raise "Unsupported relationship type #{value['type']}. Expected one of [$single, $collection]"
+              end
+
+              a.name = key
+              a.visibility = 'private'
+              a.relationship = true
+            end)
+          end
+        end
       end
 
     end
